8791e056845808445919f4ae2b52928a3d0f9047
Taylor Williams <twilliams@barkbox.com>
2019-04-22 17:19:54 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,18 +3,27 @@ $("document").ready(function() {
   // There is no content specified in the html so the page would
   // be empty if we didn't update it here.
   // Try commenting out these lines and refreshing the page!
-  updateColumn(1);
-  updateColumn(2);
-  updateColumn(3);
+    updateColumn(1);
+    updateColumn(2);
+    updateColumn(3);
+    updateColumn(4);
+
 
   $("#changeButton").click(function() {
     updateColumn(1);
     updateColumn(2);
     updateColumn(3);
+    updateColumn(4);
   });
 
  // 1.  Add a second button in the header that changes the body color of the page.
     // Call the existing randomBodyColor function to get a random color
+
+    $("#changeBody").click(function() {
+  $("body").css('background-color', randomBodyColor());
+
+    });
+
 
  // 2. Add a 4th column to the HTML. Update our page load and our click handler code to update the 4th column
 
